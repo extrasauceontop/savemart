1fa7a799f73835f266f0827586cc17e5aafe338f
extrasauceontop <msuchemba@gmail.com>
2021-05-14 14:02:21 -0400

    scrape

--- scrape.py
+++ scrape.py
@@ -8,13 +8,14 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 options = webdriver.ChromeOptions()
 
+options.add_argument("--remote-debugging-port=9222")
 options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
 options.add_argument("--disable-gpu")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 options.add_argument("start-maximized")
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 
