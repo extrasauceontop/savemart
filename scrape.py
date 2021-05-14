7faca5b2d474525677721e420bf93e48170a00d9
extrasauceontop <msuchemba@gmail.com>
2021-05-14 15:01:57 -0400

    scrape

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
 import ssl
 import time
+from sgselenium import SgChrome
 ssl._create_default_https_context = ssl._create_unverified_context
 
 options = webdriver.ChromeOptions()
@@ -18,11 +19,13 @@ options.add_argument("--headless")
 # options.add_experimental_option("excludeSwitches", ["enable-automation"])
 # options.add_experimental_option('useAutomationExtension', False)
 
-with webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options) as driver:
+# with webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options) as driver:
     # driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
     # driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
-
+with SgChrome(executable_path=ChromeDriverManager().install(), is_headless=False, chrome_options=options).driver() as driver:
+    driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     driver.get("https://www.savemart.com/stores/?coordinates=37.88151857835088,-120.41105894999998&zoom=6")
     html = driver.page_source
 
