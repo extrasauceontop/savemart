6f4a7677c1a0138f195e3f619e2c45eea37fb4b2
extrasauceontop <msuchemba@gmail.com>
2021-05-17 19:31:16 -0400

    scrape

--- scrape.py
+++ scrape.py
@@ -1,8 +1,3 @@
-# import os
-
-# os.system("pip uninstall -y sgselenium")
-# os.system("pip install sgselenium==0.0.15")
-
 from bs4 import BeautifulSoup as bs
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -28,21 +23,24 @@ latitudes = []
 longitudes = []
 hours_of_operations = []
 
+
 def get_driver(url, class_name, driver=None):
     if driver is not None:
         driver.quit()
-    
+
     user_agent = (
         "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
     )
     x = 0
     while True:
-        x = x+1
+        x = x + 1
         try:
-            driver = SgChrome(executable_path=ChromeDriverManager().install(), user_agent=user_agent, is_headless=True).driver()
-            driver.get(
-                url
-            )
+            driver = SgChrome(
+                executable_path=ChromeDriverManager().install(),
+                user_agent=user_agent,
+                is_headless=True,
+            ).driver()
+            driver.get(url)
 
             WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located((By.CLASS_NAME, class_name))
@@ -54,6 +52,7 @@ def get_driver(url, class_name, driver=None):
                 return "FAIL"
             continue
     return driver
+
 
 class_name = "store-list__store"
 url = "https://www.savemart.com/stores/?coordinates=39.64096403685537,-112.39632159999998&zoom=5"
@@ -84,7 +83,9 @@ for grid in grids:
         )
     except Exception:
         print("Getting new driver")
-        driver = get_driver(page_url, "store-details-store-hours__content", driver=driver)
+        driver = get_driver(
+            page_url, "store-details-store-hours__content", driver=driver
+        )
         print("Success")
 
     location_soup = bs(driver.page_source, "html.parser")
@@ -99,31 +100,25 @@ for grid in grids:
     ]
     city = location_soup.find("meta", attrs={"property": "og:locality"})["content"]
     state = location_soup.find("meta", attrs={"property": "og:region"})["content"]
-    zipp = location_soup.find("meta", attrs={"property": "og:postal-code"})[
+    zipp = location_soup.find("meta", attrs={"property": "og:postal-code"})["content"]
+    country_code = location_soup.find("meta", attrs={"property": "og:country-name"})[
         "content"
     ]
-    country_code = location_soup.find(
-        "meta", attrs={"property": "og:country-name"}
-    )["content"]
     store_number = location_name.split("#")[-1]
-    phone = location_soup.find("meta", attrs={"property": "og:phone_number"})[
+    phone = location_soup.find("meta", attrs={"property": "og:phone_number"})["content"]
+    location_type = "<MISSING>"
+    latitude = location_soup.find("meta", attrs={"property": "og:location:latitude"})[
         "content"
     ]
-    location_type = "<MISSING>"
-    latitude = location_soup.find(
-        "meta", attrs={"property": "og:location:latitude"}
-    )["content"]
-    longitude = location_soup.find(
-        "meta", attrs={"property": "og:location:longitude"}
-    )["content"]
+    longitude = location_soup.find("meta", attrs={"property": "og:location:longitude"})[
+        "content"
+    ]
 
     hours = ""
-    days = location_soup.find("dl", attrs={"aria-label": "Store Hours"}).find_all(
-        "dt"
+    days = location_soup.find("dl", attrs={"aria-label": "Store Hours"}).find_all("dt")
+    hours_list = location_soup.find("dl", attrs={"aria-label": "Store Hours"}).find_all(
+        "dd"
     )
-    hours_list = location_soup.find(
-        "dl", attrs={"aria-label": "Store Hours"}
-    ).find_all("dd")
 
     for x in range(len(days)):
         day = days[x].text.strip()
